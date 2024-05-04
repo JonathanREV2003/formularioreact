3f91e838fe82c4db8c48631d7e4fb921091bc7bd
Jonathan Ramiro Elías Vásquez <99297546+JonathanREV2003@users.noreply.github.com>
2024-05-04 15:54:15 -0600

    Inicial comitt

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [birthdate, setBirthdate] = useState('');
+  const [gender, setGender] = useState('');
+  const [termsAccepted, setTermsAccepted] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission here
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Nombre Completo: 
+          <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        </label>
+        <br />
+        <label>
+          Apellido Completo:
+          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </label>
+        <br />
+        <label>
+          Correo electronico:
+          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </label>
+        <br />
+        <label>
+          Numero de telefono:
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </label>
+        <br />
+        <br />
+        <label>
+          Tipo usuario:
+          <select value={gender} onChange={(e) => setGender(e.target.value)}>
+            <option value="">Asistente</option>
+            <option value="male">Expositor</option>
+            <option value="female">Voluntario</option>
+          </select>
+        </label>
+        <br />
+        <br />
+        <label>
+          Preguntas u Comentarios:
+          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </label>
+        <br />
+        <label>
+          <input type="checkbox" checked={termsAccepted} onChange={(e) => setTermsAccepted(e.target.checked)} />
+          Confirmar asistencia
+        </label>
+        <br />
+        <button type="submit">Enviar</button>
+      </form>
+    </main>
   );
 }
 
